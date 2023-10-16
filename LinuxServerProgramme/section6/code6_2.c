4b3b8b5e323d0e6140eeabc61cd641184239ccfd
yiwanming <yiwanming@yaxon.com>
2023-10-16 20:31:25 +0800

    finish code6_2

--- LinuxServerProgramme/section6/code6_2.c
+++ LinuxServerProgramme/section6/code6_2.c
@@ -10,16 +10,20 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<fcntl.h>
+#include <stdbool.h>
+#include <sys/uio.h>
 #define BUFFER_SIZE 1024
 
-int main(int argc, char **atgv)
+static const char* status_line[2]={"200 OK","500 Internal server error"};
+
+int main(int argc, char **argv)
 {
     if (argc < 4)
     {
         printf("param error!\n");
     }
     char *ip = argv[1];
-    int port = ntohs(argv[2]);
+    int port = atoi(argv[2]);
 
     int soc_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(soc_fd < 0)
@@ -30,8 +34,8 @@ int main(int argc, char **atgv)
 
 
     struct sockaddr_in server_addr;
-    server_addr.sin_family = IF_INET;
-    server_addr.sin_port = port;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
     inet_pton(AF_INET, ip, &server_addr.sin_addr);
 
     if (bind(soc_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
@@ -60,11 +64,63 @@ int main(int argc, char **atgv)
 
     //file imformation
     char handle_buf[BUFFER_SIZE];
+    memset(handle_buf, '\0', BUFFER_SIZE);
     char *file_buf;
     struct stat file_stat;
+    bool valid = true;
     int len = 0;
-    
+    const char *file_name = argv[3];
+    if(stat(file_name, &file_stat) < 0)
+    {
+        valid = false;
+    }
+    else
+    {
+        if(S_ISDIR(file_stat.st_mode))
+        {
+            valid = false;
+        }
+        else if (file_stat.st_mode & S_IROTH)
+        {
+            int fd = open(file_name, O_RDONLY);
+            file_buf =  malloc(file_stat.st_size + 1);
+            memset(file_buf, '\0', file_stat.st_size + 1);
+            if(read(fd, file_buf, file_stat.st_size) < 0)
+            {
+                valid = false;
+            }
+        }
+        else
+        {
+            valid = false;
+        }
+    }
+
+    int ret;
+    if (valid)
+    {
+        ret = snprintf(handle_buf, BUFFER_SIZE - 1, "%s%s\r\n","HTTP/1.1",status_line[0]);
+        len+=ret;
+        ret = snprintf(handle_buf+len,BUFFER_SIZE-1-len,"Content␂Length:%ld\r\n",file_stat.st_size);
+        len+=ret;
+        ret=snprintf(handle_buf+len,BUFFER_SIZE-1-len,"%s","\r\n");
+
+        struct iovec iv[2];
+        iv[0].iov_base = handle_buf;
+        iv[0].iov_len = strlen(handle_buf);
+        iv[1].iov_base = file_buf;
+        iv[1].iov_len = file_stat.st_size;
+        ret = writev(acc_fd, iv, 2);
+    }
+    else
+    {
+        ret=snprintf(handle_buf, BUFFER_SIZE-1,"%s%s\r\n","HTTP/1.1",status_line[1]);
+        len+=ret;
+        ret=snprintf(handle_buf + len,BUFFER_SIZE-1-len,"%s","\r\n");
+        send(acc_fd, handle_buf, strlen(handle_buf), 0);
+    }
 
     close(acc_fd);
-    close(soc_fd)
+    free(file_buf);
+    close(soc_fd);
 }
