e652183b22cb23ffe1a7d62d419383aaa74fb9a2
yiwanming <yiwanming@yaxon.com>
2023-11-30 21:27:41 +0800

    finish timer.hpp

--- LinuxServerProgramme/section11/11-2/timer.hpp
+++ LinuxServerProgramme/section11/11-2/timer.hpp
@@ -21,7 +21,172 @@ class util_timer;
 
 struct client_data
 {
-    sock
+    sockaddr_in address;
+    int sockfd;
+    char buf[BUFFER_SIZE];
+};
+
+class util_timer
+{
+public:
+    util_timer():prev(NULL),next(NULL) {}
+public:
+    time_t expire;
+    void *(cb_func)(client_data*);
+    client_data *user_data;
+    util_timer *prev;
+    util_timer *next;
+};
+
+class sort_timer_lst
+{
+public:
+    sort_timer_lst():head(NULL), tail(NULL){}
+    ~sort_timer_lst()
+    {
+        util_timer *tmp = head;
+        while(tmp)
+        {
+            head = tmp->next;
+            delete tmp;
+            tmp = head;
+        }
+    }
+    void add_timer(util_timer *timer)
+    {
+        if (!timer)
+        {
+            return;
+        }
+        if (!head)
+        {
+            head = tail = timer;
+            return;
+        }
+        if (timer->expire < head->expire)
+        {
+            timer->next = head;
+            timer->next->prev = timer;
+            head = timer;
+            return;
+        }
+        add_timer(timer, head);
+
+    }
+    void adjust_timer(util_timer *timer)
+    {
+        if (!timer)
+        {
+            return;
+        }
+        util_timer *tmp = timer->next;
+        if (!tmp || (timer->expire < tmp->expire))
+        {
+            return;
+        }
+        if (timer == head)
+        {
+            head = head->next;
+            head->prev = NULL;
+            timer->next = NULL;
+            add_timer(timer, head);
+        }
+        else
+        {
+            timer->prev->next = timer->next;
+            timer->next->prev = timer->prev;
+            add_timer(timer, timer->next);
+        }
+    }
+
+    void del_timer(util_timer *timer)
+    {
+        if (!timer)
+        {
+            return;
+        }
+        if ((timer == head) && (timer == tail))
+        {
+            delete timer;
+            head = NULL;
+            tail = NULL;
+            return;
+        }
+        if (timer == head)
+        {
+            head = timer->next;
+            head->prev = NULL;
+            delete timer;
+            return;
+        }
+        if (timer == tail)
+        {
+            tail = tail->prev;
+            tail->next = NULL;
+            delete timer;
+            return;
+        }
+        timer->prev->next = timer->next;
+        timer->next->prev = timer->prev;
+        delete timer;
+    }
+
+    void tick()
+    {
+        if (!head)
+        {
+            return;
+        }
+        printf("timer tick\n");
+        time_t cur = time(NULL);
+        util_timer *tmp = head;
+        while(tmp)
+        {
+            if (cur < tmp->expire)
+            {
+                break;
+            }
+            tmp->cb_func(tmp->user_data);
+            head=tmp->next;
+            if (head)
+            {
+                head->prev = NULL;
+            }
+            delete tmp;
+            tmp = head;
+        }
+    }
+
+private:
+    void add_timer(util_timer *timer, util_timer *lst_head)
+    {
+        util_timer *prev = lst_head;
+        util_timer *tmp = lst_head->next;
+        while(tmp)
+        {
+            if (timer->expire < tmp->expire)
+            {
+                timer->next = tmp;
+                timer->prev = tmp->prev;
+                timer->next->prev = timer;
+                timer->prev->next = timer;
+                break;
+            }
+            prev = tmp;
+            tmp = tmp->next;
+        }
+        if(!tmp)
+        {
+            prev->next = timer;
+            timer->next = NULL;
+            timer->prev = tail;
+            tail = timer;
+        }
+    }
+
+private:
+    util_timer *head;
+    util_timer *tail;
 };
 
 
