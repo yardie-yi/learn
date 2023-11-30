d625bd5a7a476df523976cb0725a16441e507a1f
yiwanming <yiwanming@yaxon.com>
2023-11-30 08:45:50 +0800

    11

---
+++ LinuxServerProgramme/section11/11-2/timer.hpp
@@ -0,0 +1,28 @@
+#ifndef LST_TIMER
+#define LST_TIMER
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <pthread.h>
+#define BUFFER_SIZE 64
+
+class util_timer;
+
+struct client_data
+{
+    sock
+};
+
+
+#endif
