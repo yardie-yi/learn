26c065861e877b696e6d7bc8bafd916f8a0c9d8b
yiwanming <yiwanming@yaxon.com>
2023-12-18 21:01:20 +0800

    1

--- LinuxServerProgramme/section13/13_4.c
+++ LinuxServerProgramme/section13/13_4.c
@@ -25,7 +25,72 @@ struct client_data
     sockaddr_in address;
     int connfd;
     pid_t pid;
+    int pipefd[2];
+};
 
+static const char *shm_name="/my_shm";
+int sig_pipefd[2];
+int epollfd;
+int listenfd;
+int shmfd;
+char *share_mem = 0;
+
+client_data *users = 0;
+
+int *sub_process = 0;
+
+int user_count = 0;
+
+bool stop_child = false;
+
+int setnonblocking(int fd)
+{
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;   
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+void addfd(int epollfd, int fd)
+{
+    epoll_event event;
+    event.events = EPOLLIN | EPOLLET;
+    event.data.fd = fd;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+void sig_handler(int sig)
+{
+    int save_errno = errno;
+
+    int msg = sig;
+    send(sig_pipefd[1], (char *)&msg, 1, 0);
+    errno = save_errno;
+}
+
+void addsig(int sig, void (*handler)(int), bool restart=true)
+{
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = handler;
+    if(restart)
+    {
+        sa.sa_flags |= SA_RESTART;
+    }
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+void del_resource()
+{
+    close(sig_pipefd[0]);
+    close(sig_pipefd[1]);
+    close(listenfd);
+    close(epollfd);
+    shm_unlink(shm_name);
+    delete[] users;
+    delete[] sub_process;
 }
 
 int main(int argc, char **argv)
