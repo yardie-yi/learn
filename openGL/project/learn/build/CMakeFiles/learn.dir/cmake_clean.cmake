1770088636262b60ed6b385178a4054ca007acc2
yardie <641233646@qq.com>
2024-06-08 09:45:54 +0800

    opengl init

---
+++ openGL/project/learn/build/CMakeFiles/learn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn.dir/src/test.cpp.o"
+  "CMakeFiles/learn.dir/src/test.cpp.o.d"
+  "learn"
+  "learn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
