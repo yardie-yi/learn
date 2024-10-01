171d3b76625a51e14183d8da3e6e7fbbc8b0b99e
yardie <641233646@qq.com>
2024-10-01 13:56:46 +0800

    change dir

---
+++ openGL/project/build/CMakeFiles/myOpengl.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yardie/Desktop/learn/openGL/3th/glad/src/glad.c" "CMakeFiles/myOpengl.dir/home/yardie/Desktop/learn/openGL/3th/glad/src/glad.c.o" "gcc" "CMakeFiles/myOpengl.dir/home/yardie/Desktop/learn/openGL/3th/glad/src/glad.c.o.d"
+  "/home/yardie/Desktop/learn/openGL/project/src/test.cpp" "CMakeFiles/myOpengl.dir/src/test.cpp.o" "gcc" "CMakeFiles/myOpengl.dir/src/test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ openGL/project/build/CMakeFiles/myOpengl.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myOpengl.dir/home/yardie/Desktop/learn/openGL/3th/glad/src/glad.c.o"
+  "CMakeFiles/myOpengl.dir/home/yardie/Desktop/learn/openGL/3th/glad/src/glad.c.o.d"
+  "CMakeFiles/myOpengl.dir/src/test.cpp.o"
+  "CMakeFiles/myOpengl.dir/src/test.cpp.o.d"
+  "myOpengl"
+  "myOpengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/myOpengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
